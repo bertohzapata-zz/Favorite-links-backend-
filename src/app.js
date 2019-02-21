2230b2136c48c831856f9bf3b106094cc0caffe4
Humberto Zapata León <bertohzapata@HumbertoZapata.local>
2019-02-20 18:56:07 -0600

    Configurando handlebars y middlewares: urlencoded-json()

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 // Initialization
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
+    // Handlebars settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(morgan('dev')); //dev/combined/tiny
+app.use(express.urlencoded({extended: false}));
 
 // Global variables
 
 
 // Routes
-app.use('/', (req, res) => {
-    res.json({texto: ' Hola mundo'});
-});
+app.use(require('./routes'));
 
 
 // Public
